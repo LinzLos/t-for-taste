3375fecc372d4adc1e6c8a29f7364a03bda0a156
LinzLos <223187540+LinzLos@users.noreply.github.com>
2026-09-02 19:14:53 -0700

    Chassis: Literata for voice, Courier Prime for system, Fredoka available for products; fonts self-hosted via fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import '@fontsource/literata/400.css'
+import '@fontsource/literata/400-italic.css'
+import '@fontsource/literata/700.css'
+import '@fontsource/courier-prime/400.css'
+import '@fontsource/courier-prime/700.css'
+import '@fontsource/fredoka/400.css'
+import '@fontsource/fredoka/600.css'
+import '@fontsource/fredoka/700.css'
 import './chassis/chassis.css'
 
 createRoot(document.getElementById('root')!).render(
